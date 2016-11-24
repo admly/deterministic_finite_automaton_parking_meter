68e711430ff29f9f20eb6230b0ecea84cc7927a0
admly <admly@interia.pl>
2016-11-24 18:02:36 +0100

    Version 1.0

--- src/deterministic_finite_automaton/Automaton.java
+++ src/deterministic_finite_automaton/Automaton.java
@@ -1,6 +1,6 @@
 package deterministic_finite_automaton;
-import java.util.*;
 public class Automaton {
+	private static String status = "Current state (coins thrown): ";
 	private static final  Integer[][] states = new Integer[][]{
 		{1,2,5},
 		{2,3,6},
@@ -11,48 +11,26 @@ public class Automaton {
 		{7,0,0},
 		{0,0,0},
 	};
-	
-	private final static List<Integer> coins_list = Arrays.asList(1, 2, 5);
-	private final static int sum = 0;
-	private final static int my_coin =0;
 	public static int state = 0;
+	StringBuilder string = new StringBuilder();
 	Automaton(){};
 	
-	private static boolean coin_check(int coin){
-		if(coins_list.contains(coin)){
-			return true;		}
-		else{return false;}		
-		}
-	
-	private static int define_state(int coin){
-		if(coin == 1){
-			int coin_enum = 0;
-			state=states[state][coin_enum];
-		}
-		else if (coin == 2){
-			int coin_enum = 1;
-			state=states[state][coin_enum];
-		}else{
-			int coin_enum = 2;
-			state=states[state][coin_enum];
-		}
-		if(state == 0){System.out.println("You inserted too much ! Returning coins.");}
-		return state;
-		};
+	public String define_state(int coin_enum){
+		state=states[state][coin_enum];
+		string.append(String.valueOf(state) + " ");
+		if(state == 7){
+			String message = "State achieved.\n" + "States during run: " + string + "\nActual state: " + state;
+    		return message  		
+    		;}
+    	
+    	else if(state == 0){
+    		string.setLength(0);
+    		return "Too much money, returnig your coins. Try again.";}
 		
-	public static int get_coin(){
-		 Scanner reader = new Scanner(System.in);
-		 System.out.println("Enter a coin. Accepted coins: 1, 2, 5. : ");
-	     int n = reader.nextInt(); 
-	     while(coin_check(n) == false){
-			 System.out.println("Wrong coin, try 1, 2 or 5, not " + n);
-		     n = reader.nextInt();
-		     
-	     }
-	     
-		return(define_state(n));
-	};
-		
-	
+		return String.valueOf(status + state);
+	}}
 
-}
+
+
+
+
--- src/deterministic_finite_automaton/Main.java
+++ src/deterministic_finite_automaton/Main.java
@@ -1,89 +1,55 @@
-//package deterministic_finite_automaton;
-//
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//	public static void main(String[] args) {
-//		 int state = Automaton.get_coin();
-//		 System.out.println(state);
-//		 while(state != 7){
-//		 state = Automaton.get_coin();
-//		 System.out.println(state);}
-//		 
-//
-//	}}
-
 package deterministic_finite_automaton;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Timer;
-import java.util.concurrent.TimeUnit;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
-	private static final  Integer[][] states = new Integer[][]{
-		{1,2,5},
-		{2,3,6},
-		{3,4,7},
-		{4,5,0},
-		{5,6,0},
-		{6,7,0},
-		{7,0,0},
-		{0,0,0},
-	};
-	Text T = new Text("xD");
-	private final static List<Integer> coins_list = Arrays.asList(1, 2, 5);
-	public static int state = 0;
-		
-	@Override
 	public void start(Stage primaryStage) {
 		try {
-			T.setStyle("-fx-font: 40 arial;");
+			
+			Automaton automaton = new Automaton();
+			Text initial_text = new Text("Welcome to the parking meter\nfinite automaton model.\nThrow a coin in.\n"
+					+ "You can use coins 1,2 or 5\nto achieve a sum of coins equal to 7.");
+			initial_text.setStyle("-fx-font: 40 arial;");
 			Button button1 = new Button();
 			Button button2 = new Button();
 			Button button3 = new Button();
-			button1.setText("1");
-			button2.setText("2");
-			button3.setText("5");
+			button1.setText("Throw 1 coin in");
+			button2.setText("Throw 2 coin in");
+			button3.setText("Throw 5 coin in");
 			HBox hbox = new HBox(30);
 			
 		    button1.setOnAction(new EventHandler<ActionEvent>() {
 		      public void handle(ActionEvent event) {
 		    	  int coin_enum = 0;
-				  state=states[state][coin_enum];
-				  T.setText(String.valueOf(state));
-				  check_status(state);		    	  
+		    	  String state = automaton.define_state(coin_enum);
+				  initial_text.setText(state);
+		    	  
 		      }
 		    });
 		    button2.setOnAction(new EventHandler<ActionEvent>() {
 			      public void handle(ActionEvent event) {
 			    	  int coin_enum = 1;
-					  state=states[state][coin_enum];
-					  
-					  T.setText(String.valueOf(state));
-					  check_status(state);
+			    	  String state = automaton.define_state(coin_enum);
+					  initial_text.setText(state);
+
 			      }
 			    }); 
 		    button3.setOnAction(new EventHandler<ActionEvent>() {
 			      public void handle(ActionEvent event) {
 			    	  int coin_enum = 2;
-					  state=states[state][coin_enum];
-					  
-					  T.setText(String.valueOf(state));
-					  check_status(state);
+			    	  String state = automaton.define_state(coin_enum);
+					  initial_text.setText(state);
+
 			      }
 			    });
 		       
@@ -95,8 +61,9 @@ public class Main extends Application {
 			BorderPane root = new BorderPane();
 			root.setPadding(new Insets(50));
 			root.setBottom(hbox);
-			root.setCenter(T);
-			Scene scene = new Scene(root,600,400);
+			
+			root.setCenter(initial_text);
+			Scene scene = new Scene(root,800,400);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -107,14 +74,5 @@ public class Main extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	private void check_status(int status){
-	    	if(status == 7){
-	    		T.setText("State achieved.");	    		
-	    		;}
-	    	
-	    	else if(status == 0){
-	    		T.setText("Too much cash, try again.");
-	    		;}
-	    	};
+
 }
